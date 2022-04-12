e3ee8fec8867af91589d53c79eda7552d22cef20
alebdr <alebdr@protonmail.com>
2022-04-12 13:05:48 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchModule } from './components/search/search.module';
+import { ArticleModule } from './components/article/article.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SearchModule,
+    ArticleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
